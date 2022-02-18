f12f4d28d23a0c71eb0ad9caa01218aa4750d8ec
talbotct <56003386+talbotct@users.noreply.github.com>
2022-02-18 01:18:57 -0500

    Fixed Put logic and created basic unittest

--- medAPI.py
+++ medAPI.py
@@ -86,25 +86,30 @@ class Patients(Resource):
         data = pandas.read_csv('Data.csv')
 
         # checks input patient ID if valid change the input values
-        if args['patientID'] in list(data['patientID']):
+        if int(args['patientID']) in list(data['patientID']):
+
+            # get patientID row
+            rowID = (list(data['patientID'])).index(int(args['patientID']))
+
+            # set value if new value is input
             if(args['name']):
-                (data.at[0, 'name']) = (args['name'])
+                (data.at[rowID, 'name']) = (args['name'])
             if(args['hospital']):
-                (data.at[0, 'hospital']) = (args['hospital'])
+                (data.at[rowID, 'hospital']) = (args['hospital'])
             if(args['temperature']):
-                (data.at[0, 'temperature']) = (args['temperature'])
+                (data.at[rowID, 'temperature']) = int(args['temperature'])
             if(args['systolicBP']):
-                (data.at[0, 'systolicBP']) = (args['systolicBP'])
+                (data.at[rowID, 'systolicBP']) = int(args['systolicBP'])
             if(args['diastolicBP']):
-                (data.at[0, 'diastolicBP']) = (args['diastolicBP'])
+                (data.at[rowID, 'diastolicBP']) = int(args['diastolicBP'])
             if(args['pulse']):
-                (data.at[0, 'pulse']) = (args['pulse'])
+                (data.at[rowID, 'pulse']) = int(args['pulse'])
             if(args['oximeter']):
-                (data.at[0, 'oximeter']) = (args['oximeter'])
+                (data.at[rowID, 'oximeter']) = int(args['oximeter'])
             if(args['weight']):
-                (data.at[0, 'weight']) = (args['weight'])
+                (data.at[rowID, 'weight']) = int(args['weight'])
             if(args['glucometer']):
-                (data.at[0, 'glucometer']) = (args['glucometer'])
+                (data.at[rowID, 'glucometer']) = int(args['glucometer'])
 
             # creates the updated file
             data.to_csv('Data.csv', index=False)
@@ -197,13 +202,16 @@ class Devices(Resource):
         data = pandas.read_csv('Devices.csv')
 
         # checks input patient ID if valid change the input values
-        if args['deviceID'] in list(data['deviceID']):
+        if int(args['deviceID']) in list(data['deviceID']):
+
+            rowID = (list(data['deviceID'])).index(int(args['deviceID']))
+
             if(args['deviceName']):
-                (data.at[0, 'deviceName']) = (args['deviceName'])
+                (data.at[rowID, 'deviceName']) = (args['deviceName'])
             if(args['deviceType']):
-                (data.at[0, 'deviceType']) = (args['deviceType'])
+                (data.at[rowID, 'deviceType']) = (args['deviceType'])
             if(args['manufacturer']):
-                (data.at[0, 'manufacturer']) = (args['manufacturer'])
+                (data.at[rowID, 'manufacturer']) = (args['manufacturer'])
 
             # creates the updated file
             data.to_csv('Devices.csv', index=False)
--- test_medAPI.py
+++ test_medAPI.py
@@ -1,36 +1,19 @@
 import unittest
 import medAPI
+import requests
 
 # Unit test format based on: https://www.youtube.com/watch?v=6tNS--WetLI&ab_channel=CoreySchafer
 
 class TestmedAPI(unittest.TestCase):
 
     def test_patientPost(self):
-        correctData = [11111,"Timmy Test","TestHospital",11,111,11,111,11,111,11]
-        results = medAPI.wordCount("words.txt")
+        correctTest = [11111,"Timmy Test","TestHospital",11,111,11,111,11,111,11]
 
-        self.assertDictEqual(results, correctData)
+        medAPI.post("http://127.0.0.1:5000/patients?patientID=11111&name=Timmy Test&hospital=TestHospital&temperature=11&systolicBP=111&diastolicBP=11&pulse=111&oximeter=11&weight=111&glucometer=11")
+        response = medAPI.get("http://127.0.0.1:5000/patients?patientID=12340")
 
-    def test_noFileName(self):
-        noName = "noFile"
+        self.assertDictEqual(response, correctTest)
 
-        results = medAPI.prepText("")
-
-        self.assertEqual(results, noName)
-
-    def test_emptyFile(self):
-        emp = []
-
-        results = medAPI.prepText("empty.txt")
-
-        self.assertEqual(results, emp)
-
-    def test_fileType(self):
-        fType = "formatBad"
-
-        results = word_analyzer.prepText("excel.csv")
-
-        self.assertEqual(results, fType)
 
 if __name__ == "__main__":
     unittest.main()
